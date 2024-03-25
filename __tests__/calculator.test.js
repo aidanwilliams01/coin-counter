0000a52e87d224e7ed570b41db6e04078932e130
Aidan Williams <aidanwilliams01@gmail.com>
2024-03-25 16:20:19 -0700

    add initial files

---
+++ __tests__/calculator.test.js
@@ -0,0 +1,14 @@
+import { GalacticAge } from "../src/calculator";
+
+describe('GalacticAge', () => {
+  
+  let galacticAge;
+
+  beforeEach(() => {
+    galacticAge = new GalacticAge(10);
+  });
+
+  test('should correctly create a GalacticAge object with one property', () => {
+    expect(galacticAge.earthYears).toEqual(10);
+  });
+})
