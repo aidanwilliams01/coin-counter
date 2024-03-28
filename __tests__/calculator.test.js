52e4a3d92ceb1b243edfd51248e946fd7c22ec0a
Aidan Williams <aidanwilliams01@gmail.com>
2024-03-28 13:36:20 -0700

    add tests and passing code

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -1,13 +1,24 @@
 import { coinCounter } from "../src/coin-counter";
 
 describe('coinCounter', () => {
-  const amount = 4.99;
 
   test('should return a string', () => {
-    expect(typeof coinCounter(amount)).toEqual('string');
+    expect(typeof coinCounter(4.75)).toEqual('string');
   });
 
   test('should return a string with the correct number of quarters', () => {
-    expect(coinCounter(amount)).toEqual('19 quarters');
+    expect(coinCounter(4.75)).toEqual('19 quarter(s)');
+  });
+
+  test('should return a string with the correct number of quarters and dimes', () => {
+    expect(coinCounter(4.95)).toEqual('19 quarter(s), 2 dime(s)');
+  });
+
+  test('should return a string with the correct change', () => {
+    expect(coinCounter(4.99)).toEqual('19 quarter(s), 2 dime(s), 4 pennie(s)');
+  });
+
+  test('should return a correctly displayed string when there are no quarters', () => {
+    expect(coinCounter(.24)).toEqual('2 dime(s), 4 pennie(s)');
   });
 })
--- src/coin-counter.js
+++ src/coin-counter.js
@@ -1,9 +1,28 @@
-export function coinCounter(amount) {
+export function coinCounter(amount, result) {
+    if (result === undefined) {
+        result = '';
+    }
+    amount = amount.toPrecision(3);
     const quarters = Math.floor(amount / .25);
-    // coinCounter(amount - quarters * .25);
-    // const dimes = amount / .1;
-    // const nickels = amount / .05;
-    // const pennies = amount /.01;
-
-    return `${quarters} quarters`;
+    if (quarters === 0) {
+        const dimes = Math.floor(amount / .1);
+        if (dimes === 0) {
+            const nickels = Math.floor(amount / .05);
+            if (nickels === 0) {
+                const pennies = Math.floor(amount / .01);
+                if (pennies === 0) {
+                    if (result.slice(0, 2) === ', ') {
+                        return result.slice(2);
+                    }
+                    return result;
+                }
+                return coinCounter(0, result + `, ${pennies} pennie(s)`);
+            }
+            return coinCounter(amount - nickels * .05, result + `, ${nickels} nickel(s)`);
+        }
+        else {
+            return coinCounter(amount - dimes * .1, result + `, ${dimes} dime(s)`);
+        }
+        }
+    return coinCounter(amount - quarters * .25, `${quarters} quarter(s)`);
 }
